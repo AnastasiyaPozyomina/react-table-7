bc76e1caeee71eb80dda919e30e378712b147564
AnastasiyaPozyomina <pozyomina@gmail.com>
2021-01-20 13:53:07 +0700

    настройка проекта, подготовка данных, определение столбцов, отрисовка таблицы

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, {useEffect, useState, useMemo} from 'react';
+import './App.css';
+import TableContainer from './components/TableContainer';
+
+const App = () => {
+  const [data, setData] = useState ([]);
+  useEffect (() => {
+    const doFetch = async () => {
+      const response = await fetch ('https://randomuser.me/api/?results=100');
+      const body = await response.json ();
+      const contacts = body.results;
+      console.log (contacts);
+      setData (contacts);
+    };
+    doFetch ();
+  }, []);
+
+  const columns = useMemo (
+    () => [
+      {
+        Header: 'Title',
+        accessor: 'name.title',
+      },
+      {
+        Header: 'First Name',
+        accessor: 'name.first',
+      },
+      {
+        Header: 'Last Name',
+        accessor: 'name.last',
+      },
+      {
+        Header: 'Email',
+        accessor: 'email',
+      },
+      {
+        Header: 'City',
+        accessor: 'location.city',
+      },
+    ],
+    []
+  );
+
+  return <TableContainer columns={columns} data={data} />;
+};
+
+export default App;
+
+//  useEffect() Аналогично componentDidMount и componentDidUpdate
+// Хук useMemo используется для мемоизации значений
